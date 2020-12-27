17d0566c9ea67f02230249b1fdf47deae3dc595f
Burak <karatayburakk@gmail.com>
2020-12-27 22:02:05 +0300

    Post and Get HTTP Request handled for Tasks and Users

--- src/index.js
+++ src/index.js
@@ -10,6 +10,61 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+
+app.get('/users/:id', (req,res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+
+        if(!user){
+           return res.status(404).send()
+        }
+
+        res.send(user)
+
+    })
+    .catch((e) => {
+        res.status(500).send(e)
+
+    })
+
+})
+
+app.get('/users', (req,res) => {
+
+    User.find({}).then((users) => {
+        res.send(users)
+    })
+    .catch((e) => {
+        res.send(e)
+    })
+
+})
+
+app.get('/tasks', (req,res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    })
+    .catch((error) => {
+        res.send(error)
+    })
+})
+
+app.get('/tasks/:id', (req,res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if(!task){
+            res.status(404).send()
+        }
+        res.send(task)
+    })
+    .catch((e) => {
+        res.status(500).send(e)
+    })
+
+})
+
 app.post('/tasks', (req,res) => {
 
 
