63301827dc9d40ae5c27295ff0fd1a9d4bdf206a
Burak <karatayburakk@gmail.com>
2020-12-29 02:41:20 +0300

    Authentications completed for creation, Crypted password and Login for User

--- src/routers/user.js
+++ src/routers/user.js
@@ -85,6 +85,24 @@ router.post('/users', async (req,res) => {
 
 })
 
+router.post('/users/login', async (req,res) => {
+
+    try{
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+
+    } catch(e) {
+
+        res.status(400).send(e)
+        console.log(e)
+
+    }
+
+
+
+
+})
+
 router.patch('/users/:id', async (req,res) => {
     
     const id = req.params.id
@@ -100,7 +118,13 @@ router.patch('/users/:id', async (req,res) => {
 
     try {
 
-        const user = await User.findByIdAndUpdate(id, req.body, {new:true, runValidators:true})
+        const user = await User.findById(id)
+
+        updates.forEach((update) => user[update] = req.body[update])
+
+        await user.save()
+
+        //const user = await User.findByIdAndUpdate(id, req.body, {new:true, runValidators:true})
 
         if(!user){
             return res.status(404).send()
